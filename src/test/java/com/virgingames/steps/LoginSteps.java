73a0c3e56144aec0a558c56b2f0d99dff3927656
priyankagajera <prgajera2427@gmail.com>
2024-02-02 11:18:56 +0000

    My homework

---
+++ src/test/java/com/virgingames/steps/LoginSteps.java
@@ -0,0 +1,44 @@
+package com.virgingames.steps;
+
+import com.virgingames.pages.HomePage;
+import com.virgingames.pages.LoginPage;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.testng.Assert;
+
+public class LoginSteps {
+    @Given("I am on homepage and verify the homepage URL")
+    public void iAmOnHomepageAndVerifyTheHomepageURL() {
+        new HomePage().verifyHomePage();
+    }
+
+    @And("I accept all cookies in homepage")
+    public void iAcceptAllCookiesInHomepage() {
+        new HomePage().clickOnAcceptAllCookies();
+    }
+
+    @When("I click on Login Link")
+    public void iClickOnLoginLink() {
+        new LoginPage().clickOnLoginLink();
+    }
+
+    @Then("I enter {string} Invalid Username and {string} Invalid Password for login")
+    public void iEnterInvalidUsernameAndInvalidPasswordForLogin(String username, String password) {
+        new LoginPage().sendDataToLoginFields(username , password);
+    }
+
+    @And("I click on Login Button")
+    public void iClickOnLoginButton() {
+        new LoginPage().clickOnLoginButton();
+    }
+
+    @And("I should see the error message {string}")
+    public void iShouldSeeTheErrorMessage(String message) {
+        Assert.assertEquals(new LoginPage().getErrorMessage(),"Please try again, your username/email or password has not been recognised");
+
+    }
+
+
+}
